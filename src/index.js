a861ae4e2698c2197efb5f966cc2904b2594c553
Sayan20899 <122215565+Sayan20899@users.noreply.github.com>
2024-10-11 18:20:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-// index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './styles.css';  // Import your custom styles
+import ReactDOM from 'react-dom/client';
+import './styles.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
